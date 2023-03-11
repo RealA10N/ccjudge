dae3871ce7162a301f3b5d57b3ed40ffc42f47bb
Alon Krymgand <me@alon.kr>
2023-03-11 20:57:13 +0200

    Added problem base model

---
+++ ccjudge/models/problem.py
@@ -0,0 +1,24 @@
+import datetime
+import string
+from typing import NewType
+
+from pydantic import validator
+
+from ccjudge.models.base import CCJudgeBaseModel
+
+ProblemName = NewType('ProblemName', str)
+ByteCount = NewType('ByteCount', int)
+
+
+class Problem(CCJudgeBaseModel):
+    name: ProblemName
+    timelimit: datetime.timedelta
+    memlimit: ByteCount
+
+    @validator('name')
+    def name_validator(cls, value: ProblemName) -> ProblemName:
+        VALID_CHARS = string.ascii_lowercase + '-'
+        if set(value) - set(VALID_CHARS):
+            raise ValueError(
+                "Problem name should contain lowercase letters and '-' only"
+            )
