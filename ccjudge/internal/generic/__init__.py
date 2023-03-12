8d8de8327a542408333d5871e1c4043557699f01
Alon Krymgand <me@alon.kr>
2023-03-12 16:40:32 +0200

    Added __all__ and docstring to generic init

--- ccjudge/internal/generic/__init__.py
+++ ccjudge/internal/generic/__init__.py
@@ -1,1 +1,7 @@
+""" This sub-package contains different generic interfaces that are used and
+required by CCJudge. The user (admin) has full freedom on how to implement
+those interfaces. """
+
 from .queue import CCJudgeGenericQueue
+
+__all__ = ['CCJudgeGenericQueue']
